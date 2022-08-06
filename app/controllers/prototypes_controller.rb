5548f3e4d4938551f68a0c1347cec5aead6d8499
bmm1018 <tototototoopo.mountain@gmail.com>
2022-08-06 11:10:07 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,56 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :outsider, only: [:edit, :update, :destroy]
+
+  def index
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :image, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def outsider
+    redirect_to root_path unless current_user == @prototype.user
+  end
+end
+
+
